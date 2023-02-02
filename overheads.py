8e07d4ace9ab0c5e4e1165f4266e43266148e1ba
TC01-GideonWong <124013820+TC01-GideonWong@users.noreply.github.com>
2023-02-02 16:55:56 +0800

    Update overheads.py

--- overheads.py
+++ overheads.py
@@ -18,5 +18,27 @@ def overheads_highest():
         # assigning variables for the category with the highest overhead and starting them from 0
         highest_cat = ""
         overhead = 0
+        # using a for loop to run through all the data in the file
+        for row in reader:
+            
+            # setting a condition to find the highest value amongst all the overheads, and assigning the value and respective category
+            # to variables to be printed
+            if float(row[1]) > overhead:
+                
+                highest_cat = row[0]
+                overhead = float(row[1])
+        
+        # assigning a variable to the statement showcasing the highest overhead and its category
+        write_text = (f"[HIGHEST OVERHEADS] {highest_cat.upper()}: {overhead}%")
+        
+        # returning the statement
+        return write_text
+
+# printing the function
+print(overheads_highest())
+
+# opening a txt file named "summary_report" and appending the function to the file
+with open("summary_report.txt" , "a") as file:
+    file.write(overheads_highest())
         
        
