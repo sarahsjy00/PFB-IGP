dbf2a6122367d951b08c7a283cf1e14e5f98007e
jaimietohjy <124246975+jaimietohjy@users.noreply.github.com>
2023-02-02 16:42:21 +0800

    Update mainProgram.py

--- mainProgram.py
+++ mainProgram.py
@@ -3,7 +3,14 @@ from overheads import*
 from cash_on_hand import*
 from profit_loss import*
 
-
+def main_program():
+   '''
+   overheads_highest()
+   cash_difference
+   pl_difference
+   
+# running main program
+(main_program())
 
 
     
